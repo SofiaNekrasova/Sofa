d832c4b1d5bd4cc1999becc20d1d4b21ea2f09a2
SofiaNekrasova <54958190+SofiaNekrasova@users.noreply.github.com>
2020-06-08 06:55:27 +0300

    Add files via upload

--- solution.c
+++ solution.c
@@ -7,7 +7,8 @@
 
 int count = 0; // Индекс строки в массиве   
 
-void print_text(char *dir, char text[][1000]) { // Нахождение файлов, чтение строк из файлов и запись строк в массив
+void print_text(char *dir, char text[5000][80], long int *idx) { // Нахождение файлов, чтение строк из файлов и запись строк в массив
+    char * pEnd;
     DIR *dp;
     struct dirent *entry; // Структура для работы с файловой системой
     struct stat statbuf;
@@ -22,15 +23,16 @@ void print_text(char *dir, char text[][1000]) { // Нахождение файлов, чтение стр
             /* Находит каталог, и делает рекурсивный вызов для следующего поиска */
             if (strcmp(".", entry->d_name) == 0 || strcmp("..", entry->d_name) == 0)
                continue;
-            print_text(entry->d_name, text);
+            print_text(entry->d_name, text, idx);
         }
         else //В случае, если это файл
         {
             if(strstr(entry->d_name,".txt")){
                 FILE *d = fopen(entry->d_name,"r");
                 while(!feof(d)) {
-                     fgets(text[count], 1000, d);
+                     fgets(text[count], 80, d);
                 }
+                idx[count] = strtol(text[count], &pEnd, 10);
                 count++;
                 fclose(d);
             }
@@ -40,33 +42,42 @@ void print_text(char *dir, char text[][1000]) { // Нахождение файлов, чтение стр
     closedir(dp);
 }
 
-void write_to_file(char text[][1000], long int idx[], int size) {
+void write_to_file(char text[5000][80], long int idx[], int size) {
   FILE * fp;
-  fp = fopen ("/home/box/lab3/result.txt","w");
+  fp = fopen ("./result.txt","w");
   int c = 0;
-  int ix = 0;
-  while (c<count) {
-    for (int i=0; i<count; i++) {
-      if (idx[i]==ix) {
+  int i = 0;
+    for (i=0; i<count; i++) {
          fprintf (fp, "%s\n", text[i]);
-         //ix++;
-         c++;
-      }
     }
-    ix++;
-  }
   fclose (fp);
 }
 
 int main() {
 
-    char text[1000][1000];
-    long int idx[1000];
-    print_text("./", text);
-    char * pEnd;
-    for (int i=0; i<count; i++) {
-        idx[i] = strtol(text[i], &pEnd, 10);
-    }
+    char text[5000][80]={0};
+    long int idx[5000];
+    long int tmp_idx;
+    char tmp_text[80]={0};
+    int i, j, k;
+    for (i=0; i<5000; i++)
+      idx[i] = 0;
+    print_text("./", text, idx);
+    // Отсортируем массивы
+    int c = count-1;
+    for (i=0; i<c; i++)
+        for (j=c; j>i; j--)
+            if (idx[j-1]>idx[j]) {
+               tmp_idx = idx[j-1];
+               for (k=0; k<80; k++)
+                  tmp_text[k] = text[j-1][k];
+               idx[j-1] = idx[j];
+               for (k=0; k<80; k++)
+                  text[j-1][k] = text [j][k];
+               idx[j] = tmp_idx;
+               for (k=0; k<80; k++)
+                  text[j][k] = tmp_text[k];
+            }
     write_to_file(text, idx, count);
     return 0;
 }
